075e8b18c9b95cb78286e299d64becc327f917f3
sumit <sumit81.sharma@gmail.com>
2013-07-13 20:28:22 -0700

    SumitHome Commit

--- A1-Sumit.sharma/gen/x40240/sumit/sharma/a1/app/R.java
+++ A1-Sumit.sharma/gen/x40240/sumit/sharma/a1/app/R.java
@@ -28,36 +28,51 @@ public final class R {
         public static final int white=0x7f040000;
         public static final int yellow=0x7f040001;
     }
+    public static final class dimen {
+        public static final int  padding=0x7f050003;
+        public static final int Textbox_Height=0x7f050000;
+        public static final int font_size_NextFive=0x7f050002;
+        public static final int font_size_firstFive=0x7f050001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int TextView=0x7f080000;
-        public static final int TextView2=0x7f080001;
-        public static final int menu_settings=0x7f080004;
-        public static final int text_view3=0x7f080002;
-        public static final int text_view4=0x7f080003;
+        public static final int TextView=0x7f090000;
+        public static final int TextView2=0x7f090001;
+        public static final int menu_settings=0x7f09000a;
+        public static final int text_view10=0x7f090009;
+        public static final int text_view3=0x7f090002;
+        public static final int text_view4=0x7f090003;
+        public static final int text_view5=0x7f090004;
+        public static final int text_view6=0x7f090005;
+        public static final int text_view7=0x7f090006;
+        public static final int text_view8=0x7f090007;
+        public static final int text_view9=0x7f090008;
     }
     public static final class layout {
         public static final int activity_hello_world=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_hello_world=0x7f070000;
+        public static final int activity_hello_world=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
+        public static final int S1=0x7f060003;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int menu_settings=0x7f060002;
     }
     public static final class style {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
         
 
         Base application theme for API 11+. This theme completely replaces
@@ -71,11 +86,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int MyStyle=0x7f060002;
+        public static final int AppTheme=0x7f070001;
+        public static final int MyStyle=0x7f070002;
     }
 }
